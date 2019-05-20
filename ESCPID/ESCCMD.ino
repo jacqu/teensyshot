31541ffda8f45f02bbe3995615fc5f63783ba3e2
Jacques Gangloff <jacqu@users.noreply.github.com>
2019-05-20 08:57:58 +0200

    Anti-windup PID API added.

---
+++ ESCPID/ESCCMD.ino
@@ -0,0 +1,13 @@
+/*
+ *  ESCCMD:		ESC DSHOT command packets formatting API
+ *  
+ *  Note:     Best viewed using Arduino IDE with tab space = 2
+ *
+ *  Authors:  Jacques Gangloff
+ *  Date:     May 2019
+ */
+ 
+ 
+ //
+ //	
+ //
