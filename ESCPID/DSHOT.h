8c4aa6536195171ad119ae6164750aff31609d32
Jacques Gangloff <jacqu@users.noreply.github.com>
2019-06-03 16:21:38 +0200

    Add header file for DSHOT module

---
+++ ESCPID/DSHOT.h
@@ -0,0 +1,22 @@
+/*
+ *  Definitions for DSHOT.cpp
+ *
+ */
+
+#ifndef __DSHOT_H
+#define __DSHOT_H
+
+//
+// Defines
+//
+
+#define DSHOT_MAX_OUTPUTS         ESCPID_NB_ESC // Maximum number of DSHOT outputs
+#define DSHOT_DMA_LENGTH          18            // Number of steps of one DMA sequence (the two last values are zero)
+#define DSHOT_DMA_MARGIN          2              // Number of additional bit duration to wait until checking if DMA is over
+#define DSHOT_DSHOT_LENGTH        16            // Number of bits in a DSHOT sequence
+#define DSHOT_BT_DURATION         1670          // Duration of 1 DSHOT600 bit in ns
+#define DSHOT_LP_DURATION         1250          // Duration of a DSHOT600 long pulse in ns
+#define DSHOT_SP_DURATION         625           // Duration of a DSHOT600 short pulse in ns
+
+
+#endif
