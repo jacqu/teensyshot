99b1dfcddbf851bd6b82001f95d4c723bcedfecb
Jacques Gangloff <jacqu@users.noreply.github.com>
2019-06-11 11:05:15 +0200

    Added header file for ESCPID

--- ESCPID/ESCPID.ino
+++ ESCPID/ESCPID.ino
@@ -11,18 +11,7 @@
 #include "DSHOT.h"
 #include "ESCCMD.h"
 #include "AWPID.h"
-
-// Defines
-#define ESCPID_NB_ESC             1                 // Number of ESCs
-
-#define ESCPID_USB_UART_SPEED     115200            // Baudrate of the teeensy USB serial link
-#define ESCPID_ERROR_MSG_LENGTH   80                // Max string length of an error message
-
-#define ESCPID_PID_P              1.0               // PID proportional gain
-#define ESCPID_PID_I              1.0               // PID integral gain
-#define ESCPID_PID_D              0.0               // PID derivative gain
-#define ESCPID_PID_f              0.0               // PID derivative filtering pole
-#define ESCPID_PID_SAT            999.0             // PID control input saturtion value
+#include "ESCPID.h"
 
 // Globals
 double  ESCPID_Reference[ESCPID_NB_ESC] = {};
