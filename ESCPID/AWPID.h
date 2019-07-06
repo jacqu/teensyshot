50e313de0a307e48400d638cf7c1565c71549346
Jacques Gangloff <jacqu@users.noreply.github.com>
2019-07-06 09:50:36 +0200

    Improve PID behavior after a resume from watchdog
    The internal variables of the PID are cleared when the watchdog is
    triggered. When resuming control, it suppresses unwanted transcient
    velocity bursts.

--- ESCPID/AWPID.h
+++ ESCPID/AWPID.h
@@ -11,6 +11,7 @@
 
 // Function prototypes
 void AWPID_init( uint8_t, float*, float*, float*, float*, float*, float* );
+void AWPID_reset( void );
 void AWPID_control( uint8_t, float, float, float* );
 void AWPID_tune( uint8_t, float, float, float, float, float, float );
 
